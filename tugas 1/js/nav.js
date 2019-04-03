030fe98ef25e9835aa235802c08f49818dc40583
mrayhanhakim <rayhan@pop-os.localdomain>
2019-04-03 13:00:56 +0700

    add animation to navbar

--- tugas 1/js/nav.js
+++ tugas 1/js/nav.js
@@ -1,43 +1,47 @@
 document.body.onload = function onLoad() {
+  const navBar = document.getElementById('nav');
   const navButton = document.getElementById('nav-button');
   const navMenu = document.getElementById('nav-link-wrapper');
   const leftNav = document.getElementById('left-nav');
   const rightNav = document.getElementById('right-nav');
   const navIcon = document.getElementById('nav-icon');
+  const windowTablet = window.matchMedia('(min-width: 768px)');
+  let navBarHeight;
+
   const navIconText = document.getElementById('nav-icon-text');
   navButton.addEventListener('click', () => {
     navMenu.classList.toggle('drop');
   });
 
   window.addEventListener('resize', () => {
-    if (window.matchMedia('(min-width: 768px)').matches) {
+    if (windowTablet.matches) {
       navMenu.removeAttribute('class');
     }
   });
 
   window.addEventListener('scroll', () => {
+    navBarHeight = navBar.offsetHeight;
+    navMenu.style.top = `${navBarHeight}px`;
     if (window.scrollY > 100) {
-      leftNav.style.paddingTop = '7px';
-      leftNav.style.paddingBottom = '7px';
-      rightNav.style.paddingTop = '7px';
-      rightNav.style.paddingBottom = '7px';
-      navIcon.style.width = '21px';
-      navIcon.style.height = '21px';
-      navIconText.style.fontSize = '18px';
-    } else {
-      leftNav.style.paddingTop = '20px';
-      leftNav.style.paddingBottom = '20px';
-      rightNav.style.paddingTop = '20px';
-      rightNav.style.paddingBottom = '20px';
-      if (window.matchMedia('(min-width: 768px)').matches) {
-        navIcon.style.width = '42px';
-        navIcon.style.height = '42px';
-        navIconText.style.fontSize = '26px';
-      } else {
-        navIcon.style.width = '28px';
-        navIcon.style.height = '28px';
-        navIconText.style.fontSize = '20px';
+      leftNav.classList.add('shrink');
+      rightNav.classList.add('shrink');
+      navIcon.classList.add('shrink');
+      navIconText.classList.add('shrink');
+      if (leftNav.classList.contains('unshrink')) {
+        leftNav.classList.remove('unshrink');
+        rightNav.classList.remove('unshrink');
+        navIcon.classList.remove('unshrink');
+        navIconText.classList.remove('unshrink');
       }
+    } else {
+      leftNav.classList.remove('shrink');
+      rightNav.classList.remove('shrink');
+      navIcon.classList.remove('shrink');
+      navIconText.classList.remove('shrink');
+      leftNav.classList.add('unshrink');
+      rightNav.classList.add('unshrink');
+      navIcon.classList.add('unshrink');
+      navIconText.classList.add('unshrink');
     }
   });
 };
