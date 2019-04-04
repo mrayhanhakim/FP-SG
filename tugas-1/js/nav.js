e9a711cf550e99b0d728dac750dc9b177e8d9260
mrayhanhakim <rayhan@pop-os.localdomain>
2019-04-04 22:47:51 +0700

    Rename folder

---
+++ tugas-1/js/nav.js
@@ -0,0 +1,51 @@
+document.body.onload = function onLoad() {
+  const navBar = document.getElementById('nav');
+  const navButton = document.getElementById('nav-button');
+  const navMenu = document.getElementById('nav-link-wrapper');
+  const leftNav = document.getElementById('left-nav');
+  const rightNav = document.getElementById('right-nav');
+  const navIcon = document.getElementById('nav-icon');
+  const windowTablet = window.matchMedia('(min-width: 768px)');
+  let navBarHeight = navBar.offsetHeight;
+  navMenu.style.top = `${navBarHeight}px`;
+
+  const navIconText = document.getElementById('nav-icon-text');
+  navButton.addEventListener('click', () => {
+    navMenu.classList.toggle('drop');
+  });
+
+  window.addEventListener('resize', () => {
+    if (windowTablet.matches) {
+      navMenu.removeAttribute('class');
+    }
+  });
+
+  window.addEventListener('scroll', () => {
+    navBarHeight = navBar.offsetHeight;
+    navMenu.style.top = `${navBarHeight}px`;
+    if (window.scrollY > 100) {
+      navBar.classList.add('shrink');
+      leftNav.classList.add('shrink');
+      rightNav.classList.add('shrink');
+      navIcon.classList.add('shrink');
+      navIconText.classList.add('shrink');
+      if (leftNav.classList.contains('unshrink')) {
+        navBar.classList.remove('unshrink');
+        leftNav.classList.remove('unshrink');
+        rightNav.classList.remove('unshrink');
+        navIcon.classList.remove('unshrink');
+        navIconText.classList.remove('unshrink');
+      }
+    } else {
+      leftNav.classList.remove('shrink');
+      rightNav.classList.remove('shrink');
+      navIcon.classList.remove('shrink');
+      navIconText.classList.remove('shrink');
+      navBar.classList.add('unshrink');
+      leftNav.classList.add('unshrink');
+      rightNav.classList.add('unshrink');
+      navIcon.classList.add('unshrink');
+      navIconText.classList.add('unshrink');
+    }
+  });
+};
